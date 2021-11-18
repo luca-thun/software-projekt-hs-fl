0a626d306c972a32732c1d21a20119eab4b18307
luca-thun <luca.thun@stud.hs-flensburg.de>
2021-11-18 17:41:23 +0100

    Back - Verknüpfungen eingerichtet

--- Lernprogramm/Lernprogramm/MainWindow.xaml.cs
+++ Lernprogramm/Lernprogramm/MainWindow.xaml.cs
@@ -25,48 +25,85 @@ namespace Lernprogramm
             InitializeComponent();
         }
 
+        public void AllesVerstecken()
+        {
+            GridStartseite.Visibility = Visibility.Hidden;
+            GridMenu.Visibility = Visibility.Hidden;
+            GridSchwierigkeit.Visibility = Visibility.Hidden;
+            GridAufgabenmenge.Visibility = Visibility.Hidden;
+        }
+
         private void Start_Click(object sender, RoutedEventArgs e)
         {
+            Titel.Text = "STARTSEITE";
+            AllesVerstecken();
+            GridStartseite.Visibility = Visibility.Visible;
 
+            Start.Visibility = Visibility.Hidden;
+            Menu.Visibility = Visibility.Hidden;
         }
         private void Menu_Click(object sender, RoutedEventArgs e)
         {
+            Titel.Text = "MENÜ";
+            AllesVerstecken();
+            GridMenu.Visibility = Visibility.Visible;
 
+            Start.Visibility = Visibility.Visible;
+            Menu.Visibility = Visibility.Hidden;
         }
 
         private void ButtonStartseite_Click(object sender, RoutedEventArgs e)
         {
+            Titel.Text = "MENÜ";
+            AllesVerstecken();
+            GridMenu.Visibility = Visibility.Visible;
 
+            Start.Visibility = Visibility.Visible;
+            Menu.Visibility = Visibility.Hidden;
         }
 
         private void PlusRechnen_Click(object sender, RoutedEventArgs e)
         {
+            Titel.Text = "PLUS RECHNEN";
+            AllesVerstecken();
+            GridSchwierigkeit.Visibility = Visibility.Visible;
 
+            Menu.Visibility = Visibility.Visible;
         }
 
         private void MinusRechnen_Click(object sender, RoutedEventArgs e)
         {
+            Titel.Text = "MINUS RECHNEN";
+            AllesVerstecken();
+            GridSchwierigkeit.Visibility = Visibility.Visible;
 
+            Menu.Visibility = Visibility.Visible;
         }
 
         private void ZahlenSortieren_Click(object sender, RoutedEventArgs e)
         {
+            Titel.Text = "ZAHLEN SORTIEREN";
+            AllesVerstecken();
 
+            Menu.Visibility = Visibility.Visible;
         }
 
         private void Einfach_Click(object sender, RoutedEventArgs e)
         {
-
+            AllesVerstecken();
+            GridAufgabenmenge.Visibility = Visibility.Visible;
         }
 
         private void Mittel_Click(object sender, RoutedEventArgs e)
         {
-
+            AllesVerstecken();
+            GridAufgabenmenge.Visibility = Visibility.Visible;
         }
 
         private void Schwer_Click(object sender, RoutedEventArgs e)
         {
-
+            AllesVerstecken();
+            GridAufgabenmenge.Visibility = Visibility.Visible;
         }
 
         private void Aufgaben5_Click(object sender, RoutedEventArgs e)
